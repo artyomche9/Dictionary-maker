3105d373e500f1b69f4e569df94a222037377fee
artyomche9 <artyomche9@gmail.com>
2018-08-21 12:18:54 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+from collections import Counter
+from googletrans import Translator
+import requests
+from googletrans import Translator
+
+
+translator = Translator()
+def Translate(arr):
+    rus = []
+    eng = []
+    for i in arr:
+        try:
+            translation = translator.translate(i, dest='ru')
+            eng.append(translation.origin)
+            rus.append(translation.text)
+            print(translation.origin, ' -> ', translation.text)
+        except:
+            pass
+        
+
+
+
+translator = Translator()
+
+f = open('Alice.txt', 'r')
+text = f.read()
+division = []
+for i in text:
+    if not (i.isalpha() or i =="'"):
+        division.append(i)
+
+for i in division:
+    text = text.replace(i,' ')
+text = text.lower()
+array = text.split()
+
+unique = []
+for i in array:
+   if array.count(i) == 1:
+       unique.append(i)
+print(len(unique))
+unique.sort()
+Translate(unique)
